f44715f38df000bbe5ad30959af82503c1eea339
karim <@1>
2023-05-03 00:37:00 +0300

    Update news popup.

--- app/src/main/java/com/example/myapplication/NewsPopup.kt
+++ app/src/main/java/com/example/myapplication/NewsPopup.kt
@@ -1,10 +1,13 @@
 package com.example.myapplication
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
@@ -15,12 +18,40 @@ class NewsPopup(
     private val description: String
 ) : DialogFragment() {
 
+    override fun onStart() {
+        super.onStart()
+
+        val dialog = dialog
+        dialog?.window?.setLayout(
+            getDialogMaxWeight(),
+            getDialogMaxHeight()
+        )
+    }
+
+    private fun getDialogMaxHeight(): Int {
+        val displayMetrics = resources.displayMetrics
+        val screenHeight = displayMetrics.heightPixels
+
+        return (screenHeight * 0.75).toInt()
+    }
+
+    private fun getDialogMaxWeight(): Int {
+        val displayMetrics = resources.displayMetrics
+        val screenWeight = displayMetrics.widthPixels
+
+        return (screenWeight * 0.9).toInt()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.news_dialog, container, false)
+        val view = inflater.inflate(R.layout.news_dialog, container, false)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
